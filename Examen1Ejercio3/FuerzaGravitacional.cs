ff77bc84acfc6045596bb31be8d7650d5746d162
salasjc <salasjc@HRD6402574>
2024-02-17 04:44:57 -0600

    Add project files.

---
+++ Examen1Ejercio3/FuerzaGravitacional.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1Ejercio3
+{
+    class FuerzaGravitacional
+    {
+         double masa1;
+         double masa2;
+         double distancia;
+         const double G = 6.673E-11; // Constante de gravitación 
+
+
+        //constructores
+        public double Masa1
+        {
+            get { return masa1; }
+            set { masa1 = value; }
+        }
+
+        public double Masa2
+        {
+            get { return masa2; }
+            set { masa2 = value; }
+        }
+
+        public double Distancia
+        {
+            get { return distancia; }
+            set { distancia = value; }
+        }
+
+        // este metodo realiza la operacion, simulando la formula
+        public double CalcularFuerza()
+        {
+            return (G * Masa1 * Masa2) / (Distancia * Distancia);
+        }
+
+    }
+}
---
+++ Examen1Ejercio3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Examen1Ejercio3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //solicita los datos al usuario, indicando su unidad de medida.
+            Console.WriteLine("Ingrese la masa 1 (en kg):");
+            double masa1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la masa 2 (en kg):");
+            double masa2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la distancia entre las masas (en metros):");
+            double distancia = Convert.ToDouble(Console.ReadLine());
+
+
+            // Crea una nueva instancia de la clase FuerzaGravitacional . Esto crea un nuevo objeto de tipo FuerzaGravitacional y almacena una referencia  en la variable fuerza.
+            FuerzaGravitacional fuerza = new FuerzaGravitacional();
+            fuerza.Masa1 = masa1;
+            fuerza.Masa2 = masa2;
+            fuerza.Distancia = distancia;
+            
+            
+            //Llama al método CalcularFuerza del objeto fuerza para calcular la fuerza de atracción entre las masas, y el resultado se almacena en la variable fuerzaAtraccion.
+
+            double fuerzaAtraccion = fuerza.CalcularFuerza();
+            Console.WriteLine($"La fuerza de atracción entre las masas es: {fuerzaAtraccion} N");
+        }
+    }
+    
+}
